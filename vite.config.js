fb9bfb70fa7bab607c004ad75f7ee1ca830eb8ec
shafiqahmad81 <shafiqahmad081@gmail.com>
2025-02-26 12:07:03 +0600

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,14 @@
 import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+        whatscbd: path.resolve(__dirname, 'whats-cbd.html'),
+      },
+    },
+  },
   plugins: [
     tailwindcss(),
   ],
