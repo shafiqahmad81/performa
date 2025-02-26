9a96df4906f89605ee8c0d6094b4f9b7bade0325
shafiqahmad81 <shafiqahmad081@gmail.com>
2025-02-26 12:23:23 +0600

    update vite config file

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,8 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: path.resolve(__dirname, '/index.html'),
-        about: path.resolve(__dirname, '/whats-cbd.html'),
+        main: path.resolve(__dirname, 'index.html'),
+        about: path.resolve(__dirname, 'whats-cbd.html'),
       },
     },
   },
